0b6b9d257495253ddcbf2c8dd65d7f26349a381c
islam.ar <islam.ahmed@approcks.com>
2021-08-11 16:27:42 +0200

    Test Searching

--- app/src/androidTest/java/com/islam/githubapp/ui/mainscreen/MainFragmentTest.kt
+++ app/src/androidTest/java/com/islam/githubapp/ui/mainscreen/MainFragmentTest.kt
@@ -1,33 +1,24 @@
 package com.islam.githubapp.ui.mainscreen
 
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.View
-import androidx.lifecycle.lifecycleScope
+import android.widget.EditText
 import androidx.recyclerview.widget.RecyclerView
-import androidx.test.InstrumentationRegistry.getTargetContext
-import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.BoundedMatcher
-import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
-import androidx.test.platform.app.InstrumentationRegistry
 import com.islam.githubapp.R
-import com.islam.githubapp.data.repositories.FakeMainRepositoryUITest
 import com.islam.githubapp.launchFragmentInHiltContainer
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.test.runBlockingTest
 import org.hamcrest.Matcher
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -42,25 +33,18 @@ class MainFragmentTest {
     @get:Rule
     var hiltRule = HiltAndroidRule(this)
 
-    private lateinit var viewModel: MainViewModel
-
     @Before
     fun setup() {
-
         launchFragmentInHiltContainer<MainFragment>(Bundle(), R.style.Theme_MyTask)
-
     }
 
     @Test
     fun launchFragmentReturnsStarterImageAtFirstLaunch() {
-
         onView(withId(R.id.starterImage)).check(matches(isDisplayed()))
-
     }
 
     @Test
     fun loadsTheDefaultResults() {
-
         onView(withId(R.id.list)).check { view, noViewFoundException ->
             if (noViewFoundException != null) {
                 throw noViewFoundException
@@ -73,11 +57,43 @@ class MainFragmentTest {
 
     @Test
     fun checkToolbarSearchIcon() {
-
         onView(withId(R.id.search)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun searchForNameReturns() {
+
+        onView(withId(R.id.search)).perform(click())
+        onView(isAssignableFrom(EditText::class.java)).perform(
+            typeText("name_1"),
+            pressKey(KeyEvent.KEYCODE_ENTER)
+        )
+        onView(withId(R.id.list)).check(
+            matches(
+                atPosition(
+                    0,
+                    hasDescendant(withText("name_1"))
+                )
+            )
+        )
 
     }
 
+    private fun atPosition(position: Int, itemMatcher: Matcher<View?>): Matcher<View?> {
+        return object : BoundedMatcher<View?, RecyclerView>(RecyclerView::class.java) {
 
+            override fun matchesSafely(view: RecyclerView): Boolean {
+                val viewHolder = view.findViewHolderForAdapterPosition(position)
+                    ?: // has no item on such position
+                    return false
+                return itemMatcher.matches(viewHolder.itemView)
+            }
+
+            override fun describeTo(description: org.hamcrest.Description?) {
+                description?.appendText("has item at position $position: ")
+                itemMatcher.describeTo(description)
+            }
+        }
+    }
 
 }
